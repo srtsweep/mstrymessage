13546cd59721d7e39d71ff66c8efad311765164e
Mayank gupta <nisebhayop.69@gmail.com>
2025-12-30 19:34:00 +0530

    logout fix-2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           session ? (
             <>
               <span className="mr-4">Welcome , {user?.username || user?.email}</span>
-              <Link href='/'>
+              <Link href='/dashboard'>
                 <Button /*onClick={() => signOut()}*/ className="w-full md:w-auto bg-slate-100 text-black" >
                   Logout
                 </Button>
